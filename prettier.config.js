790cbfb708ea8347f263acc53222256d3cdfcedb
Nenad Vajagic <50087069+nvajagic@users.noreply.github.com>
2024-02-10 18:20:51 +0100

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  useTabs: false,
+  trailingComma: "all",
+  singleQuote: false,
+  semi: true,
+};
+
+export default config;
+`
